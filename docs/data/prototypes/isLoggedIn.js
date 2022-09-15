e7a59ca67e596acad88c75cdc1c196b79958147e
Christodoulos Ioannou <cioannou@eey.gov.cy>
2022-09-15 09:33:52 +0300

    Ιστορικό ολοκληρωμένων αιτήσεων
    Προσθήκη οθονών ιστορικού ολοκληρωμένων αιτήσεων
    Αλλαγή τρόπου επεξεργασίας ενεργών αιτήσεων

--- docs/data/prototypes/isLoggedIn.js
+++ docs/data/prototypes/isLoggedIn.js
@@ -30,6 +30,12 @@ var currentRouteId;
 var currentRouteNo;
 var positionSelectedString;
 
+function openPage(name) {
+
+    var newURL= "#p/promotions" + '/' + name;
+    window.location=newURL;
+}
+
 function forwardPageOnRoute(steps) {
 
 	var nextRoutId=parseInt(currentRouteNo) + steps;
@@ -53,8 +59,18 @@ function selectPositionString(id) {
 		positionSelectedString = "Deputy Principal A' Secondary General Education for Informatics - Computer Science";
 	else if (id == 5)
 		positionSelectedString = "Inspector Secondary General Education for Informatics - Computer Science";
-	else
+	else if (id == 6)
 		positionSelectedString = "Pedagogical Institute Teacher for Informatics";
+	else if (id == 11)
+		positionSelectedString = "Βοηθός Διευθυντής Α' Μέσης Γενικής Εκπαίδευσης για τη Πληροφορική - Επιστήμη Η.Υ.";
+	else if (id == 12)
+		positionSelectedString = "Καθηγητής Παιδαγωγικού Ινστιτούτου για τη Πληροφορική";
+	else if (id == 13)
+		positionSelectedString = "Deputy Principal Secondary General Education for Informatics - Computer Science";
+	else if (id == 14)
+		positionSelectedString = "Pedagogical Institute Teacher for Informatics";
+	else
+		positionSelectedString = "Wrong position ID. Cannot retreive position description";
 }
 
 function selectPosition(id) {
@@ -66,5 +82,9 @@ function selectPosition(id) {
 function viewPosition(id) {
 
 	selectPositionString(id);
-	forwardPageOnRoute(4);
+	if (id <= 10)
+		forwardPageOnRoute(4);
+	else
+		openPage("scenarioCheckHistory");
+		
 }
